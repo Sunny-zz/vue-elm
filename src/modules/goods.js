be71b0de7af39f0773188b10be418ec79d844532
Sunny-zz <b695340402@163.com>
2018-09-17 09:51:31 +0800

    update

--- src/modules/goods.js
+++ src/modules/goods.js
@@ -12,7 +12,7 @@ const mutations = {
 
 const actions = {
   getGoods({ commit }) {
-    const uri = 'http://pezl7mzdp.bkt.clouddn.com/db.json'
+    const uri = 'https://raw.githubusercontent.com/Sunny-zz/vue-elm/master/api/db.json'
     axios.get(uri).then(res => {
       // console.log(res.data)
       commit('getGoods', res.data.goods)
